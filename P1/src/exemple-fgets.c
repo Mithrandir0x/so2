b3a27a7784f0ae5b27a1f3cb9545c0b5e75d9c41
Mithrandir0x <mithrandir0x@gmail.com>
2014-09-28 10:42:44 +0200

    Added implementation to extract words from file.

--- P1/src/exemple-fgets.c
+++ P1/src/exemple-fgets.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_LENGTH 10
+
+void original_readfile(char *str, FILE *fp);
+void corrected_readfile(char *str, FILE *fp);
+
 int main(void)
 {
-  char str[100];
+  char str[BUFFER_LENGTH];
   FILE *fp;
 
   fp = fopen("fitxer.txt", "r");
@@ -12,13 +17,29 @@ int main(void)
     exit(1);
   }
 
-  fgets(str, 100, fp);
-  while (!feof(fp)) {
-     printf("Linia: %s\n", str);
-     fgets(str, 100, fp);
-  }
+  original_readfile(str, fp);
+
+  corrected_readfile(str, fp);
 
   fclose(fp);
 
   return 0;
+}
+
+void corrected_readfile(char *str, FILE *fp) {
+  printf("Corrected version that allows to print all the file:\n");
+  
+  while ( fgets(str, BUFFER_LENGTH, fp) != NULL ) {
+    printf("Linia: %s\n", str);
+  }
+}
+
+void original_readfile(char *str, FILE *fp) {
+  printf("Original version:\n");
+  
+  fgets(str, BUFFER_LENGTH, fp);
+  while ( !feof(fp) ) {
+    printf("Linia: %s\n", str);
+    fgets(str, BUFFER_LENGTH, fp);
+  }
 }
---
+++ P1/src/extract.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define BUFFER_LENGTH 100
+
+typedef void (*F_PTR)(char*);
+
+// ASCII TABLE (PUNCTUATION GRAPHS)
+//   (Source: // http://www.cplusplus.com/reference/cctype/)
+
+//  !    "    #    $    %    &    '    (    )    *    +    ,    -    .    /
+// 0x21 0x22 0x23 0x24 0x25 0x26 0x27 0x28 0x29 0x2A 0x2B 0x2C 0x2D 0x2E 0x2F
+
+//  :    ;    <    =    >    ?    @
+// 0x3A 0x3B 0x3C 0x3D 0x3E 0x3F 0x40
+
+//  [    \    ]    ^    _    `
+// 0x5B 0x5C 0x5D 0x5E 0x5F 0x60
+
+#define IS_WRD_CONCATENATOR(x) x == 0x27 || x == 0x2D
+
+void print_word(char* str);
+void clear_buffer(char *b);
+int read_text_file(FILE *stream, F_PTR f);
+
+int main()
+{
+  FILE *fp;
+
+  fp = fopen("fitxer.txt", "r");
+  if (!fp) {
+    printf("No he pogut obrir fitxer.\n");
+    exit(1);
+  }
+
+  read_text_file(fp, &print_word);
+
+  fclose(fp);
+
+  return 0;
+}
+
+int read_text_file(FILE *stream, F_PTR f)
+{
+  char str[BUFFER_LENGTH];
+  char wrd[BUFFER_LENGTH];
+
+  // i: index of the char index from "str"
+  // j: index of the NEXT char to add in "wrd"
+  int i = 0, j = 0;
+  
+  while ( fgets(str, BUFFER_LENGTH, stream) != NULL )
+  {
+    i = 0;
+    while ( str[i] != 0 )
+    {
+      if ( isalpha(str[i]) || isdigit(str[i]) || IS_WRD_CONCATENATOR(str[i]) ) {
+        wrd[j] = str[i];
+        j++;
+      } else
+      
+      if ( ispunct(str[i]) ) {
+        if ( j > 0 ) f(wrd);
+        clear_buffer(wrd);
+        j = 0;
+      } else if ( isspace(str[i]) ) {
+        if ( j > 0 ) f(wrd);
+        clear_buffer(wrd);
+        j = 0;
+      }
+
+      else {
+        wrd[j] = str[i];
+        j++;
+      }
+
+      i++;
+    }
+  }
+
+  return 0;
+}
+
+void clear_buffer(char *b) {
+  int i;
+  for ( i = 0 ; i < BUFFER_LENGTH ; i++ )
+    b[i] = 0;
+}
+
+void print_word(char* str)
+{
+  printf("%s\n", str);
+}
